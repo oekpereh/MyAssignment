9799cb74f3d4df05ed709b3501e41c190cae889e
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-03-28 00:06:22 -0400

    comment added

--- src/missingFront.java
+++ src/missingFront.java
@@ -4,8 +4,7 @@
  * version 1.0
  * Course: ITEC 2140 Spring 2023
  * Written: March 27, 2023
- * This java class contains the completed methods and test
- * by calling the methods with appropriate inputs in the main method.
+ * This java program will return a version without the first 3 chars, given a string.
  */
 
 public class missingFront {
