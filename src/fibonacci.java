d0a5c3b0b66e8fd252c7416b92ccec5ea7742985
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-03-27 22:38:46 -0400

    code created comment added

---
+++ src/fibonacci.java
@@ -0,0 +1,25 @@
+/**
+ * @Class: fibonacci
+ * @author: Olufunmilayo Ekpereh
+ * version 1.0
+ * Course: ITEC 2140 Spring 2023
+ * Written: March 27, 2023
+ * This java class contains the completed methods and test
+ * by calling the methods with appropriate inputs in the main method.
+ */
+
+public class fibonacci {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(0));
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+    }
+}
+
